687ce4c22787507faa6a5c973c7107fb8e96cd88
Joël Troch <joel.troch62@gmail.com>
2014-11-17 16:57:34 +0100

    Création de la classe "Utilisateur".

---
+++ src/com/seuxmaximetrochjoel/applicationfredi/Utilisateur.java
@@ -0,0 +1,14 @@
+package com.seuxmaximetrochjoel.applicationfredi;
+
+public class Utilisateur {
+	
+	private String nom;
+	
+	public Utilisateur(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+}
