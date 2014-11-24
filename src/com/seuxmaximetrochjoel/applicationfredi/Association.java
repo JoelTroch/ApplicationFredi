15f1f7a8696b58faeb51af49751eaf20a532f9d2
Joël Troch <joel.troch62@gmail.com>
2014-11-24 14:21:02 +0100

    [Association] Suite de la rédaction de la classe.

--- src/com/seuxmaximetrochjoel/applicationfredi/Association.java
+++ src/com/seuxmaximetrochjoel/applicationfredi/Association.java
@@ -7,12 +7,30 @@ public class Association {
 	// ====================================================================================================
 	
 	private long id;
+	private String nom;
 	
 	// ====================================================================================================
 	// METHODES
 	// ====================================================================================================
 	
-	public Association(long id) {
+	public Association(long id, String nom) {
 		this.id = id;
+		this.nom = nom;
+	}
+	
+	public long getId() {
+		return this.id;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 }
