3da41f024d9830eaa1f41b8833933240e0490d70
JoelTroch <joel.troch62@gmail.com>
2014-11-26 12:05:36 +0100

    [BDHelper] Préparatifs pour la classe "Deplacement"

--- src/com/seuxmaximetrochjoel/applicationfredi/BDHelper.java
+++ src/com/seuxmaximetrochjoel/applicationfredi/BDHelper.java
@@ -11,6 +11,7 @@ public class BDHelper extends SQLiteOpenHelper {
 	// ====================================================================================================
 	
 	private final String requeteCreationTableAssociation = "CREATE TABLE association(_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom TEXT NOT NULL);";
+	private final String requeteCreationTableDeplacement = "CREATE TABLE deplacement(_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date DATE NOT NULL, motif TEXT NOT NULL, intituleTrajet TEXT NOT NULL, nbKm FLOAT NOT NULL, montantPeage FLOAT NOT NULL, montantRepas FLOAT NOT NULL, montantHebergement FLOAT NOT NULL, idAssociation INTEGER NOT NULL, FOREIGN KEY(idAssociation) REFERENCES association(_id));";
 	private final String requeteCreationTableUtilisateur = "CREATE TABLE utilisateur(nom TEXT PRIMARY KEY NOT NULL, prenom TEXT NOT NULL, adresse TEXT NOT NULL, ville TEXT NOT NULL, cp TEXT NOT NULL);";
 	
 	// ====================================================================================================
@@ -28,6 +29,7 @@ public class BDHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase bdd) {
 		bdd.execSQL(requeteCreationTableAssociation);
+		bdd.execSQL(requeteCreationTableDeplacement);
 		bdd.execSQL(requeteCreationTableUtilisateur);
 	}
 	
@@ -36,6 +38,7 @@ public class BDHelper extends SQLiteOpenHelper {
 		/* TODO : Si on a le temps, faire en sorte de conserver les données lors d'une mise à jour de la base
 		 * de données. Ca serait frustant pour l'utilisateur de perdre ses données à cause de ça.
 		 */
+		bdd.execSQL("DROP TABLE deplacement;");
 		bdd.execSQL("DROP TABLE association;");
 		bdd.execSQL("DROP TABLE utilisateur;");
 		onCreate(bdd);
