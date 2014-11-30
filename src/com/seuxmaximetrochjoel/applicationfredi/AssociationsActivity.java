f733392036a4624cbcb8c611113ec35e51d4dcfe
Maxime-s <seux.maxime@gmail.com>
2014-12-01 00:07:57 +0100

    Début du menu contextuel pour AssociationsActivity

--- src/com/seuxmaximetrochjoel/applicationfredi/AssociationsActivity.java
+++ src/com/seuxmaximetrochjoel/applicationfredi/AssociationsActivity.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -44,6 +49,7 @@ public class AssociationsActivity extends Activity {
 		miseAJourListe();
 		listeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listeAssociationsAffichage);
 		listViewAssociations.setAdapter(listeAdapter);
+		registerForContextMenu(listViewAssociations);
 	}
 	
 	private void miseAJourListe() {
@@ -53,5 +59,27 @@ public class AssociationsActivity extends Activity {
 		manipBDD.close();
 		for (Association uneAssociation : listeAssociations)
 			listeAssociationsAffichage.add(uneAssociation.getNom());
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		if (v.getId() == R.id.listViewAssociations) {
+			// Ajout des actions au menu contextuel
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+			menu.setHeaderTitle(listeAssociationsAffichage.get(info.position));
+			menu.add(Menu.NONE, 0, 0, "Modifier");
+			menu.add(Menu.NONE, 1, 1, "Effacer");
+		}
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+		if (item.getItemId() == 0) { // Modifier
+			// TODO : Afficher l'activité "Créer une association" en mode modification
+		} else { // Effacer
+			// TODO : Effacer tous les déplacements de l'associations et ensuite effacer cette dernière
+		}
+		return true;
 	}
 }
